3207da0edc68eab86470d31e787fee84c42880a4
sheeeee94 <afk.sh94@gmail.com>
2017-12-30 23:52:27 +0900

    448. Find All Numbers Disappeared in an Array 공간복잡도 O(1)로 풀기
    value가 나왔다면 그 인덱스의 value를 negative로 해서 나왔음을 표시.
    두번째 순회에서 인덱스의 값이 양수라면 그 인덱스에 값은 배열에 없다는 것을 의미.

--- leetcode/easy/FindAllNumbersDisappearedInAnArray.java
+++ leetcode/easy/FindAllNumbersDisappearedInAnArray.java
@@ -3,16 +3,12 @@ import java.util.*;
 public class FindAllNumbersDisappearedInAnArray {
 	public List<Integer> findDisappearedNumbers(int[] nums) {
 		List<Integer> result = new ArrayList<>();
-		int[] appear = new int[nums.length + 1];
-
-		for (int n : nums) {
-			appear[n] = 1;
+		for(int i=0; i<nums.length;i++){
+			int n=Math.abs(nums[i])-1;
+			if(nums[n]>0)nums[n]=-nums[n];
 		}
-		
-		for (int i = 1; i <= nums.length; i++)
-			if (appear[i] == 1)
-				result.add(i);
-
+		for(int i=0; i<nums.length;i++)
+			if(nums[i]>0)result.add(i+1);
 		return result;
 	}
 }
