3555b210a4683a89476c4e5968ea5d12717a3a91
sheeeee94 <afk.sh94@gmail.com>
2017-12-30 19:55:03 +0900

    136. Single Number bit연산
    bit연산이 짱되네;
    xor연산을 통해 두번 나온 것들은 0이 되고 한 번 나온것만 남게 됨.

--- leetcode/easy/SingleNumber.java
+++ leetcode/easy/SingleNumber.java
@@ -1,14 +1,7 @@
-import java.util.*;
-
 public class SingleNumber {
 	public int singleNumber(int[] nums) {
-		Map<Integer,Boolean> appear=new HashMap<>();
-		for(int n:nums){
-			if(appear.containsKey(n))appear.remove(n);
-			else appear.put(n, true);
-		}
-		int result=0;
-		for(int key:appear.keySet())result=key;
-		return result;
+		int res=0;
+		for(int n:nums)res=res^n;
+		return res;
 	}
 }
