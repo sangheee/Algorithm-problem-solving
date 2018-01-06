74f915a2f7cd6403338b0c698c71b06aa2092d3b
sheeeee94 <afk.sh94@gmail.com>
2018-01-06 19:41:45 +0900

    169. Majority Element
    배열 정렬때문에 무조건 O(nlogn)이상인데 정렬 없이 O(n)에 풀 수 없을까?

---
+++ leetcode/easy/MajorityElement.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MajorityElement {
+	public int majorityElement(int[] nums) {
+		Arrays.sort(nums);
+		int cnt=0;
+		int major=nums[0];
+		for(int n: nums){
+			if(cnt==0)major=n;
+			cnt+=major==n?1:-1;
+		}
+		return major;
+	}
+}
