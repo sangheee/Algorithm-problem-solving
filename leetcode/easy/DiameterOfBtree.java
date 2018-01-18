49268df329da8834a1d7c8d0a295de8209d5d124
sheeeee94 <afk.sh94@gmail.com>
2018-01-18 22:43:36 +0900

    543. Diameter of Binary Tree
    중복 계산 제거

--- leetcode/easy/DiameterOfBtree.java
+++ leetcode/easy/DiameterOfBtree.java
@@ -1,12 +1,16 @@
 
 public class DiameterOfBtree {
 	public int diameterOfBinaryTree(TreeNode root) {
-		if(root==null)return 0;
-		return Math.max(Math.max(diameterOfBinaryTree(root.left),diameterOfBinaryTree(root.right)), getDiameter(root.left,0)+getDiameter(root.right,0));
+		getDiameter(root);
+		return max;
 	}
-	public int getDiameter(TreeNode root, int sum){
-		if(root==null)return sum;
-		return Math.max(getDiameter(root.left, sum+1),getDiameter(root.right, sum+1));
-		
+	private int max=0;
+	public int getDiameter(TreeNode root){
+		if(root==null)return 0;
+		int left=getDiameter(root.left);
+		int right=getDiameter(root.right);
+		int sum=Math.max(left, right);
+		max=Math.max(max, left+right);
+		return sum+1;
 	}
 }
