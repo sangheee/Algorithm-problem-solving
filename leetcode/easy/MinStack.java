93558cae549c7b73f39a09d5dbe08943c456c4da
sheeeee94 <afk.sh94@gmail.com>
2018-01-21 02:42:29 +0900

    155. Min Stack
    use two stack(stack + dp cache)

---
+++ leetcode/easy/MinStack.java
@@ -0,0 +1,22 @@
+
+public class MinStack {
+	public MinStack() {
+
+	}
+
+	public void push(int x) {
+
+	}
+
+	public void pop() {
+
+	}
+
+	public int top() {
+
+	}
+
+	public int getMin() {
+
+	}
+}
