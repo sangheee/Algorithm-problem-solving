ce896a36c5c2097bcf7a7abc91d50a7a965a136d
sheeeee94 <afk.sh94@gmail.com>
2018-01-21 02:43:07 +0900

    155. Min Stack
    use two stack(stack + dp cache)
    저장 안한지 모르고 올려벌임...다시 올리기

--- leetcode/easy/MinStack.java
+++ leetcode/easy/MinStack.java
@@ -1,22 +1,31 @@
-
+import java.util.*;
 public class MinStack {
+	private LinkedList<Integer> stack=new LinkedList<>();
+	private LinkedList<Integer> minStack=new LinkedList<>();
+	
 	public MinStack() {
 
 	}
 
 	public void push(int x) {
-
+		stack.add(x);
+		
+		if(minStack.size()<1)minStack.add(x);
+		else minStack.add(minStack.peekLast()<x?minStack.peekLast():x);
 	}
 
 	public void pop() {
-
+		stack.removeLast();
+		minStack.removeLast();
+		
 	}
 
 	public int top() {
-
+		if(stack.size()<1) return 0;
+		return stack.getLast();
 	}
 
 	public int getMin() {
-
+		return minStack.getLast();
 	}
 }
