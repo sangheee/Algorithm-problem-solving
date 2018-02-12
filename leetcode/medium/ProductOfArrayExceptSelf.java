c9f940b91e65c7d999c7c6ebc113a670d00d7df5
sanghee <afk.sh94@gmail.com>
2018-02-13 01:21:57 +0900

    238. Product of Array Except Self
    원소가 1개 이상인 배열 nums가 있을때,
     output배열을 반환한다.
    output배열은 output[i]번째가 nums[i]번째 원소를 제외한 모든 nums 원소의 합인 배열이다.
    예를 들어, nums=[1,2,3,4] 라고 하면
    output=[2x3x4, 1x3x4, 1x2x4,1x2x3] =[24,12,8,6]
    을 반환하는 문제이다.
    
    이 문제는 left와 right를 나누어 해결하는 문제이다.
    
    즉, 첫번째 반복문에서는 앞에서부터 자신을 제외한 자신까지의 수들을 곱하고, 두번째 반복문에서 뒤에서부터 자신을 제외한 자신까지의 수들을 곱하는 것이다.
    
    이해하기 쉽게 예를 들면,
    nums=[1,2,3,4]일 때, 첫번째 반복문에서
    output=[1(default),1,1x2,1x2x3] 을 계산하고 두번째 반복문에서는 이 계산된 값에 뒤에서부터 값을 곱해준다.
    즉,
    output2(2번째 for문이란 뜻이다)
    =[output[0]*output2[1],output[1]*output2[2],output[2]*output2[3],output[3]*1(default)]

---
+++ leetcode/medium/ProductOfArrayExceptSelf.java
@@ -0,0 +1,15 @@
+
+public class ProductOfArrayExceptSelf {
+	public int[] productExceptSelf(int[] nums) {
+		int[] result=new int[nums.length];
+		for(int i=0, tmp=1;i<nums.length;i++) {
+			result[i]=tmp;
+			tmp*=nums[i];
+		}
+		for(int i=nums.length-1,tmp=1;i>=0;i--) {
+			result[i]*=tmp;
+			tmp*=nums[i];
+		}
+		return result;
+	}
+}
