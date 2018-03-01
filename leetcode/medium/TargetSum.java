1ed50f67a22656c498259c24db7783280964c98f
sanghee <afk.sh94@gmail.com>
2018-03-01 23:39:05 +0900

    494. Target Sum solve using dp

--- leetcode/medium/TargetSum.java
+++ leetcode/medium/TargetSum.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class TargetSum {
 
-	public int findTargetSumWays(int[] nums, int S) {	
+/*	public int findTargetSumWays(int[] nums, int S) {	
 		int step=0;
 		
 		Map<Integer,Integer>cnt=new HashMap<>();
@@ -24,9 +24,28 @@ public class TargetSum {
 		}
 		return cnt.getOrDefault(S, 0);		
 	}
+	*/
+	public int findTargetSumWays(int[] nums, int S) {
+		int sum=0;
+		for(int n:nums)sum+=n;
+		if(S>sum || S<-sum)return 0;
+		int[]dp=new int[2*sum+1];
+		dp[sum]=1;
+		for(int i=0;i<nums.length;i++) {
+			int[]next=new int[2*sum+1];
+			for(int j=0; j<2*sum+1;j++) {
+				if(dp[j]!=0) {
+					next[j+nums[i]]+=dp[j];
+					next[j-nums[i]]+=dp[j];
+				}
+			}
+			dp=next;
+		}
+		return dp[sum+S];
+	}
 	@Test
 	public void test() {
-		//assertEquals(findTargetSumWays(new int[] {1},2),0);
+		assertEquals(findTargetSumWays(new int[] {1},2),0);
 		assertEquals(findTargetSumWays(new int[] {1,1,1,1,1},3),5);
 	}
 }
