fc58b62fd1b703483ac50414b5bde98defa0526d
sanghee <afk.sh94@gmail.com>
2018-03-02 21:28:27 +0900

    62. Unique Paths
    전통적 최적화 문제

---
+++ leetcode/medium/UniquePaths.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UniquePaths {
+	public int uniquePaths(int m, int n) {
+		int[][]paths=new int[m][n];
+		
+		for(int i=0;i<m;i++)paths[i][0]=1;
+		for(int i=0;i<n;i++)paths[0][i]=1;
+		for(int i=1;i<m;i++) {
+			for(int j=1;j<n;j++)
+				paths[i][j]=paths[i-1][j]+paths[i][j-1];
+		}
+		return paths[m-1][n-1];
+	}
+	@Test
+	public void test() {
+		assertEquals(uniquePaths(1,2),1);
+		assertEquals(uniquePaths(3,7),28);
+	}
+}
