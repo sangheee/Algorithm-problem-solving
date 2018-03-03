5b1af4951f9919d40eb2eb4c081b2b355e2103b3
sanghee <afk.sh94@gmail.com>
2018-03-03 18:53:37 +0900

    560. Subarray Sum Equals K
    무식하게풀기

---
+++ leetcode/medium/SubarraySumEqualsK.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import java.util.*;
+import org.junit.Test;
+
+public class SubarraySumEqualsK {
+	public int subarraySum(int[] nums, int k) {
+		int n=nums.length;
+		int[]preSum=new int[n+1];
+		Map<Integer,Integer> appear=new HashMap<>();
+		for(int i=0;i<n;i++)preSum[i+1]=preSum[i]+nums[i];
+		int[] preSum2=new int[n+1];
+		for(int i=0;i<n+1;i++)preSum2[i]=preSum[i]-k;
+		int res=0;
+		for(int i=0;i<n+1;i++) {
+			if(appear.containsKey(preSum2[i]))res+=appear.get(preSum2[i]);
+			int v=appear.getOrDefault(preSum[i], 0);
+			appear.put(preSum[i], v+1);
+		}
+		return res;
+	}
+	@Test
+	public void test() {
+		assertEquals(7,subarraySum(new int[] {1,1,1,1,1,-1,-1,1},2));
+		assertEquals(5,subarraySum(new int[] {1,1,1,1,1,1},2));
+	}
+}
