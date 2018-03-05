8ba7ca2a2e570ea4c530af96111458c0a4b7657b
sanghee <afk.sh94@gmail.com>
2018-03-05 21:25:26 +0900

    309. Best Time to Buy and Sell Stock with Cooldown
    공간최적화

--- leetcode/medium/BestTimeToBuyAndSellStockWithCooldown.java
+++ leetcode/medium/BestTimeToBuyAndSellStockWithCooldown.java
@@ -3,13 +3,17 @@ public class BestTimeToBuyAndSellStockWithCooldown {
 	public int maxProfit(int[] prices) {
 		int n=prices.length;
 		if(n<1)return 0;
-		int[] have=new int[n+1];
-		int[] nothave=new int[n+1];
-		have[1]=-prices[0];
+		int have=-prices[0],prehave=0;
+		int nothave=0,prenothave=0;
+		
 		for(int i=2; i<n+1;i++) {
-			have[i]=Math.max(have[i-1], nothave[i-2]-prices[i-1]);
-			nothave[i]=Math.max(nothave[i-1], have[i-1]+prices[i-1]);
+			int tmp=have;
+			have=Math.max(have, prenothave-prices[i-1]);
+			prehave=tmp;
+			tmp=nothave;
+			nothave=Math.max(nothave, prehave+prices[i-1]);
+			prenothave=tmp;
 		}
-		return nothave[n];
+		return nothave;
 	}
 }
