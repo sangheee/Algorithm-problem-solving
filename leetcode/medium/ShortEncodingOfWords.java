428fd6be03ed95a001453fdaef5baa14c25ebc60
sanghee <afk.sh94@gmail.com>
2018-05-05 22:14:59 +0900

    820. Short Encoding of Words using hashset
    처음에 set에 스트링배열 원소를 모두 추가하고,
    각각의 스트링을 순회하면서 스트링원소의 섭셋을 셋에서 제거
    
    최종적으로 셋에 남은 원소들의 길이의 합을 구한다.

--- leetcode/medium/ShortEncodingOfWords.java
+++ leetcode/medium/ShortEncodingOfWords.java
@@ -4,8 +4,9 @@ import org.junit.Test;
 import java.util.*;
 
 public class ShortEncodingOfWords {
+	/*
 	public int minimumLengthEncoding(String[] words) {
-		PriorityQueue<String> pq=new PriorityQueue<String>((o1,o2)-> {return o2.length()-o1.length();});
+		PriorityQueue<String> pq=new PriorityQueue<>((o1,o2)-> {return o2.length()-o1.length();});
 		for(String word:words)pq.add(word);
 		StringBuilder builder=new StringBuilder();
 		while(!pq.isEmpty()) {
@@ -16,8 +17,25 @@ public class ShortEncodingOfWords {
 		}
 		return builder.length();
 	}
+	*/
+
+	public int minimumLengthEncoding(String[] words) {
+		Set<String> wordSet=new HashSet<>(Arrays.asList(words));
+		for(String word:words) {
+			for(int i=1;i<word.length();i++) {
+				wordSet.remove(word.substring(i));
+			}
+		}
+		int ret=0;
+		for(String word:wordSet) {
+			ret+=word.length()+1;
+		}
+		return ret;
+	}
+	
 	@Test
 	public void test() {
 		assertEquals(10, minimumLengthEncoding(new String[]{"time","me","bell"}));
 	}
+
 }
